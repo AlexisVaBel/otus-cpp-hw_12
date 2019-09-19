46738cf617be46fa57f1eac97206e8d60e6f8d73
lebllex <alexisvabel@gmail.com>
2019-09-19 16:53:45 +0700

    Corrected work

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/shared_ptr.hpp>
+
+
+
+
+#include "server/server.h"
+
+int main(int argc, char ** argv){
+
+    if(argc < 3) throw std::length_error("need port parameter and bulksize parameter");
+
+    size_t portNum  = std::stoi(argv[1]);
+    size_t bulkSize = std::stoi(argv[2]);
+
+
+    boost::shared_ptr<boost::asio::io_service> m_io_service(
+                new boost::asio::io_service);
+
+
+
+    Server s(m_io_service, portNum, bulkSize); // param here
+    m_io_service->run();
+
+
+
+
+    return 0;
+}
